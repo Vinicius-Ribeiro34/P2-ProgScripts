8a536e3621d436fc16504873f9e2896492083aef
Vinicius de Campos Ribeiro <66170007+Vinicius-Ribeiro34@users.noreply.github.com>
2020-11-29 07:32:28 -0300

    JS02 parte 2 pronta

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,89 @@
 const express = require("express");
 const router = express.Router();
 
+const Category = require("./Category");
+
+const slugify = require("slugify");
+
 router.get("/categories", (req, res) => {
     res.send("ROTA DE CATEGORIAS");
 });
 
 router.get("/admin/categories/new", (req, res) => {
-    res.send("ROTA PARA CRIAR NOVA CATEGORIA");
+    res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    var title = req.body.title;
+    if (title != undefined) {
+        Category.create({ //insert into
+            title: title,
+            slug: slugify(title) //Title -> "Programação de Scripts" com slug vai transformar em "programacao-de-scripts" para ser possível usar nas rotas
+        }).then(() => {
+            res.redirect("/admin/categories");
+        });
+    }
+    else {
+        res.redirect("admin/categories/new");
+    }
+});
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", { categories: categories });
+    });
+});
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id;
+    if (id != undefined) {   //se id != null
+        if (!isNaN(id)) {       //se id não for número
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }
+    else {
+        res.redirect("/admin/categories");
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category });
+        }
+        else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    });
+});
+
+//criando a rota para update
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    Category.update({ title: title, slug: slugify(title) }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    });
 });
 
 module.exports = router;

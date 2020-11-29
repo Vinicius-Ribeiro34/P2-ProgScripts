c84b9b2f4a4728395ed8f308f420ed9cf44dd99a
Vinicius de Campos Ribeiro <66170007+Vinicius-Ribeiro34@users.noreply.github.com>
2020-11-29 06:20:50 -0300

    js01 completo

---
+++ categories/Category.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+const Article = require("../articles/Article")
+const connection = require("../database/database");
+
+const Category = connection.define('categories', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+//Category.sync({force:true});
+
+module.exports = Category;
